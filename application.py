474348ca980e796d084e4381427a4b8435dba1a9
Ryan Ngai <ryanngai21@gmail.com>
2024-10-24 22:05:50 -0400

    works locally but not on ebs

--- application.py
+++ application.py
@@ -6,39 +6,24 @@ import pickle
 
 application = Flask(__name__)
 
-@application.route("/")
-def index():
-    return "Your Flask App Works! V1.0"
+# @application.route("/", methods=["GET"])
+# def index():
+#     return "Your Flask App Works! V2.0"
 
+
+@application.route("/", methods=["GET", "POST"])
 def load_model():
-    with open('basic_classifier.pkl', 'rb') as fid:
-        loaded_model = pickle.load(fid)
-    with open('count_vectorizer.pkl', 'rb') as vd:
-        vectorizer = pickle.load(vd)
-    return loaded_model, vectorizer
+    with open("basic_classifier.pkl", "rb") as model_file:
+        model = pickle.load(model_file)
+    with open("count_vectorizer.pkl", "rb") as vectorizer_file:
+        vectorizer = pickle.load(vectorizer_file)
+    inputs = [
+        "This is fake news",
+        "Homeowners who regularly rent on Airbnb and other sites must pay 13% tax on property value when they sell, recent tax ruling finds",
+    ]
+    predictions = model.predict(vectorizer.transform(inputs))
+    return jsonify({"predictions": predictions.tolist()})
 
-# model, vectorizer = load_model()
-
-def fetch_article_content(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.text
-    else:
-        return None
-
-# @application.route("/predict", methods=["POST"])
-# def predict():
-#     data = request.get_json()
-#     if 'url' not in data:
-#         return jsonify({"error": "No URL provided"}), 400
-    
-#     url = data['url']
-#     article_content = fetch_article_content(url)
-#     if article_content is None:
-#         return jsonify({"error": "Unable to fetch article content"}), 400
-    
-#     prediction = model.predict(vectorizer.transform([article_content]))[0]
-#     return jsonify({"prediction": prediction})
 
 if __name__ == "__main__":
-    application.run(port=5000, debug=True)
+    application.run()
